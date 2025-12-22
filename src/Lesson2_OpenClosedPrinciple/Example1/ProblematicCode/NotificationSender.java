5db6fd5284d1e20c857158d28b4356bae43485d2
Nandini390 <23bit150@ietdavv.edu.in>
2025-12-23 02:00:43 +0530

    Added lesson 2 BetterCode

--- src/Lesson2_OpenClosedPrinciple/Example1/ProblematicCode/NotificationSender.java
+++ src/Lesson2_OpenClosedPrinciple/Example1/ProblematicCode/NotificationSender.java
@@ -17,3 +17,11 @@ public class NotificationSender {
     }
 
 }
+
+//OPEN-CLOSED Principle:- A class should be open for extension but closed for modifications
+
+//Problems
+//1. This class knows too much about notifications i.e. notification types and their methods
+//2. Extendibility:- as new notification method is added in NotificationType class then code of this class also has to be change
+//Hence we are not able to modify/add new features without modifying the class
+//This condition violates the OpenClosedPrinciple
